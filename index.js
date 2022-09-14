d4312e09150e856212e495e4afa3cbb5a154917c
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-09-14 19:00:19 -0400

    corrected file save location

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(data){
     return new Promise((resolve, reject) => {
-        fs.writeFile("./Develop/README.md", data, err => {
+        fs.writeFile("./README.md", data, err => {
             if (err) {
                 reject(err);
                 return;
